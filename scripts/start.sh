6f923276ff73632a10f4be1d12abc4fd9c16456d
boateng0303 <boatengkwasi0303@gmail.com>
2024-12-15 11:41:54 -0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --name=netflix  -p 8080:80  dhruvdarji123/netflix-react-app:latest 
+docker run -d --name=netflix  -p 8080:80  boatengkwasi1991/netflix-react-app:latest 
