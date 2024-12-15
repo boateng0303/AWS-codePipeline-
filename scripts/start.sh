26faae6ba4ad239875b4703bf3c077a053e3f20f
boateng0303 <boatengkwasi0303@gmail.com>
2024-12-15 12:47:25 -0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-# Pull the latest Docker image
-docker pull boatengkwasi1991/netflix-react-app:latest || {
-    echo "Failed to pull Docker image. Exiting."
-    exit 1
-}
-
-# Run the container
-docker run -d --name=netflix -p 8080:80 boatengkwasi1991/netflix-react-app:latest || {
-    echo "Failed to start Docker container. Exiting."
-    exit 1
-}
-
-echo "Docker container started successfully."
+docker run -d --name=netflix  -p 8080:80  dhruvdarji123/netflix-react-app:latest 
