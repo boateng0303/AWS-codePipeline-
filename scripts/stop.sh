6834ee61c8db5594a183cc7677a81092e99e5a57
boateng0303 <boatengkwasi0303@gmail.com>
2024-12-15 12:47:59 -0500

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Stop the container if it exists
-docker stop netflix 2>/dev/null || echo "Container 'netflix' is not running. Skipping stop."
-
-# Remove the container if it exists
-docker rm netflix 2>/dev/null || echo "Container 'netflix' does not exist. Skipping removal."
-
-# Remove the Docker image if it exists
-docker rmi boatengkwasi1991/netflix-react-app:latest 2>/dev/null || echo "Image 'boatengkwasi1991/netflix-react-app:latest' does not exist. Skipping image removal."
-
-echo "Cleanup completed."
+docker stop netflix
+docker rm netflix
+docker image rm dhruvdarji123/netflix-react-app:latest 
